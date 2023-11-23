884bc63f7971dd857e37578bf447e8ec3508b393
ThinhTruong12 <thinhtruong042@gmail.com>
2023-11-23 14:57:53 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.wc8lxrz.mongodb.net',
+  DB_USER: 'thinhtruong042',
+  DB_PASS: 'thinhtruong2009',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'thinhtruong',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'thinhtruong042@gmail.com', // gmail service
+  EMAIL_PASS: 'haff cpas gjjk jnxx',
+};
+module.exports = MyConstants;
